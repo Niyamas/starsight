d3b4f88c3e7036adddbe6819646e40c58020cc22
Andrei Teleron <ateleron2@gmail.com>
2020-11-30 17:37:19 -0600

    Updated apod

--- home/static/home/js/external_apis.js
+++ home/static/home/js/external_apis.js
@@ -15,7 +15,24 @@ class ExternalAPIS {
 
             console.log(`apod data structure = ${apod}`)
 
+            let descriptionFirst = apod.explanation.substring(0, 300)
+            let descriptionSecond = apod.explanation.substring(300)
+
             // Store HTML image for APOD
+            /* let apodHTML = `
+                <h2 class="apod__banner">
+                    Astronomy Picture of the Day
+                    <span class="apod__date">${apod.date}</span>
+                </h2>
+                <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
+                    <img class="apod__img" src="${apod.url}" alt="NASA's Astronomy Picture of the Day (APOD)"></img>
+                </a>
+                <h3>
+                    ${apod.title}
+                    <span class="apod__copyright">by ${apod.copyright}</span>
+                </h3>
+                <p class="apod__description">${apod.explanation}</p>
+            ` */
             let apodHTML = `
                 <h2 class="apod__banner">
                     Astronomy Picture of the Day
@@ -28,7 +45,11 @@ class ExternalAPIS {
                     ${apod.title}
                     <span class="apod__copyright">by ${apod.copyright}</span>
                 </h3>
-                <p>${apod.explanation}</p>
+                <p class="apod__description">
+                    ${apod.explanation}
+                    <a class="apod__description__fade" href="#"></a>
+                </p>
+                <div class="apod__read-more">Read more</div>
             `
 
             // Place apodHTML in innerHTML of div with ID='apod'
