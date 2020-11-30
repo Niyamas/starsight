54206ba20f1adda2ed6e9848069d81e0cb34f067
Andrei Teleron <ateleron2@gmail.com>
2020-11-29 22:47:01 -0600

    spruced up the css

--- home/models.py
+++ home/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 from wagtail.core.models import Page
 
+from decouple import config
+
 
 class HomePage(Page):
-    pass
+    """Home page model"""
+
+    templates = 'home/home_page.html'
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+        context['nasa_api_key'] = config('NASA_API_KEY')
+        return context
---
+++ home/static/home/js/external_apis.js
@@ -0,0 +1,34 @@
+class ExternalAPIS {
+
+    constructor() {
+
+    }
+
+    static async getAPOD() {
+        /**
+         * 
+         */
+        let url = 'https://api.nasa.gov/planetary/apod?api_key=' + nasaAPIKey
+        await fetch(url)
+        .then( (response) => response.json() )
+        .then( (apod) => {
+
+            console.log(`apod data structure = ${apod}`)
+
+            // Store HTML image for APOD
+            let apodHTML = `
+                <h2>NASA's Astronomy Picture of the Day</h2>
+                <h3>${apod.title}</h3>
+                <p>${apod.date}</p>
+                <img src="${apod.hdurl}" alt="NASA's Astronomy Picture of the Day (APOD)"></img>
+                <p>${apod.explanation}</p>
+            `
+
+            // Place apodHTML in innerHTML of div with ID='apod'
+            document.getElementById('apod').innerHTML = apodHTML
+
+        })
+    }
+}
+
+ExternalAPIS.getAPOD()
