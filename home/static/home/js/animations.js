eeee920490b739c51c1394c111e522e5a5879e03
Andrei Teleron <ateleron2@gmail.com>
2020-12-20 18:56:47 -0600

    Created a working sticky header

--- home/static/home/js/animations.js
+++ home/static/home/js/animations.js
@@ -4,17 +4,44 @@ class Animations {
 
     }
 
-    static addReadMore() {
+    static nav() {
         /**
          * 
          */
 
-        const readMore = document.getElementById('readMore')
+        console.log('Running Animations.nav()')
 
-        readMore.addEventListener('click', () => {
+        const nav = document.getElementById('nav')
+        const navStarsight = document.getElementById('navStarsight')
+        const navLinks = Array.from( document.getElementsByClassName('nav__links__link') )
 
-            console.log('read more clicked!')
+        window.addEventListener('scroll', () => {
+            //console.log('scrollY = ', window.scrollY)
+
+            if (window.scrollY > 70) {
+
+                nav.classList.add('sticky')
+                navStarsight.classList.add('sticky')
+
+                navLinks.forEach( (link) => {
+
+                    link.classList.add('sticky')
+                })
+            }
+            else {
+
+                nav.classList.remove('sticky')
+                navStarsight.classList.remove('sticky')
+
+                navLinks.forEach( (link) => {
+
+                    link.classList.remove('sticky')
+                })
+            }
         })
     }
+
+
 }
 
+Animations.nav()
