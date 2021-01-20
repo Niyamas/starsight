ba1b1c274680951575a570c5e69b5122a1b76e97
Andrei Teleron <ateleron2@gmail.com>
2021-01-19 20:37:25 -0600

    updated wsgi

--- starsight/wsgi.py
+++ starsight/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "starsight.settings.dev")
 
 application = get_wsgi_application()
+
+# See: https://wagtail.io/blog/deploying-wagtail-heroku/
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
