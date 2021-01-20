b7f815a7702c3cd0060c33cdb2f61535d6250bb4
Andrei Teleron <ateleron2@gmail.com>
2021-01-19 22:35:58 -0600

    s

--- starsight/settings/production.py
+++ starsight/settings/production.py
@@ -30,8 +30,8 @@ COMPRESS_CSS_HASHING_METHOD = 'content'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Let static files be served by whitenoise
-#django_heroku.settings(locals())                    # @39:00: https://www.youtube.com/watch?v=6DI_7Zja8Zc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=17
-django_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals())                    # @39:00: https://www.youtube.com/watch?v=6DI_7Zja8Zc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=17
+#django_heroku.settings(locals(), staticfiles=False)
 
 
 
